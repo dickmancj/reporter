12dd4fe3110cb4e843c3116a0aa4aafdab682599
ericsvendsen <esvendsen@gmail.com>
2016-09-14 16:50:34 -0400

    added file upload and form submit capabilities

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import Dropzone from 'react-dropzone';
 import ReportList from './ReportList';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import DatePicker from 'material-ui/DatePicker';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+import './Form.css';
 
 class Form extends Component {
   constructor(props) {
@@ -18,9 +22,11 @@ class Form extends Component {
       product_id: '',
       publish_date: new Date(),
       updated_date: new Date(),
-      report_type: ''
+      report_type: '',
+      report_files: []
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(key, value) {
@@ -30,6 +36,12 @@ class Form extends Component {
       // callback after state changes
       console.log(this.state[key]);
     });
+  }
+
+  handleClick() {
+    // stringify state for form submission
+    let formData = JSON.stringify(this.state);
+    console.log(formData);
   }
 
   render() {
@@ -52,24 +64,6 @@ class Form extends Component {
                 <MenuItem value="Type 4" primaryText="Type 4" />
               </SelectField>
             </div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">
-              <TextField id="title" onChange={(event) => { this.handleChange('title', event.target.value); }} floatingLabelText="Title"/>
-            </div>
-            <div className="col">
-              <TextField id="author" onChange={(event) => { this.handleChange('author', event.target.value); }} floatingLabelText="Author"/>
-            </div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">
-              <TextField id="url" onChange={(event) => { this.handleChange('url', event.target.value); }} floatingLabelText="URL"/>
-            </div>
-            <div className="col">
-              <TextField id="product-id" onChange={(event) => { this.handleChange('product_id', event.target.value); }} floatingLabelText="Product ID"/>
-            </div>
-          </div>
-          <div className="flex-grid">
             <div className="col">
               <DatePicker id="publish-date" container="inline" mode="landscape" floatingLabelText="Publish Date" autoOk={true} value={this.state.publish_date} onChange={(event, date) => { this.handleChange('publish_date', date); }}/>
             </div>
@@ -79,11 +73,37 @@ class Form extends Component {
           </div>
           <div className="flex-grid">
             <div className="col">
+              <TextField id="title" onChange={(event) => { this.handleChange('title', event.target.value); }} floatingLabelText="Title"/>
+            </div>
+            <div className="col">
+              <TextField id="author" onChange={(event) => { this.handleChange('author', event.target.value); }} floatingLabelText="Author"/>
+            </div>
+            <div className="col">
+              <TextField id="url" onChange={(event) => { this.handleChange('url', event.target.value); }} floatingLabelText="URL"/>
+            </div>
+            <div className="col">
+              <TextField id="product-id" onChange={(event) => { this.handleChange('product_id', event.target.value); }} floatingLabelText="Product ID"/>
+            </div>
+          </div>
+          <div className="flex-grid">
+            <div className="col">
               <TextField id="description" onChange={(event) => { this.handleChange('description', event.target.value); }} multiLine={true} rows={3} floatingLabelText="Description"/>
             </div>
             <div className="col">
               <TextField id="keyword-list" onChange={(event) => { this.handleChange('keyword_list', event.target.value); }} multiLine={true} rows={3} floatingLabelText="Keywords"/>
             </div>
+            <div className="col">
+              <Dropzone className="dropzone" onDrop={(files) => { this.handleChange('report_files', files); }}>
+                <div>Try dropping some files here, or click to select files to upload.</div>
+                {this.state.report_files ? <div>
+                  <small>Uploading {this.state.report_files.length} files...</small>
+                  <div>{this.state.report_files.map((file, idx) => <p key={idx}>{file.name}</p>)}</div>
+                </div> : null}
+              </Dropzone>
+            </div>
+          </div>
+          <div className="submit-btn">
+            <RaisedButton label="Submit" fullWidth={true} labelPosition="before" primary={true} icon={<FontIcon className="material-icons">check_circle</FontIcon>} onClick={this.handleClick}/>
           </div>
           <ReportList source="http://localhost:9200/reports/document/_search?q=Research%20Report%202&pretty=true"/>
         </div>
