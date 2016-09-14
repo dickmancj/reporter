30a83cfe2f2060f747be8a044812ac485e5da181
Chris Dickman <chris.dickman@appliedis.com>
2016-09-14 15:02:33 -0400

    added bash script and config to start local ES docker container

---
+++ search/run-es.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#echo "The script you are running has basename `basename $0`, dirname `dirname $0`"
+#echo "The present working directory is `pwd`"
+scriptdir=`dirname "$BASH_SOURCE"`
+cd $scriptdir
+
+docker run -d -v "$PWD/esdata":/usr/share/elasticsearch/data -v "$PWD":/usr/share/elasticsearch/config -p 9200:9200 -p 9300:9300 elasticsearch
