7fb2b09a95d0c58452fa45d4877d283d1a1172fb
Chris Dickman <chris.dickman@appliedis.com>
2016-09-15 10:24:32 -0400

    updated dockerfile to include mapper-attachments

--- search/run-es.sh
+++ search/run-es.sh
@@ -4,4 +4,4 @@
 scriptdir=`dirname "$BASH_SOURCE"`
 cd $scriptdir
 
-docker run -d -v "$PWD/esdata":/usr/share/elasticsearch/data -v "$PWD/config":/usr/share/elasticsearch/config -p 9200:9200 -p 9300:9300 elasticsearch
+docker run -d -v "$PWD/esdata":/usr/share/elasticsearch/data -v "$PWD/config":/usr/share/elasticsearch/config -p 9200:9200 -p 9300:9300 dickmancj/elasticsearch
