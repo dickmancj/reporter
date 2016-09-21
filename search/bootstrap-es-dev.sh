7f00a82349e06b1be8f3ed1821724416ea2bd1c5
Chris Dickman <chris.dickman@appliedis.com>
2016-09-21 07:56:23 -0400

    updated search bootstrap script with new fields. this is useful when connecting GeoServer to new ES index w/o data.

---
+++ search/bootstrap-es-dev.sh
@@ -0,0 +1,24 @@
+curl -X POST 'http://localhost:9200/reports' -d '{
+    "settings" : {}
+    }'
+
+curl -H 'Content-Type: application/json' -H 'Accept: application/json' -X PUT 'http://localhost:9200/reports/report/_mapping' -d '{
+    "report" : {
+        "properties" : {
+            "classification": {"type": "string" },
+            "title": { "type": "string" },
+            "description": { "type": "string" },
+            "keyword_list": { "type": "string" },
+            "url": { "type": "string" },
+            "author": { "type": "string" },
+            "publish_date": { "type": "date" },
+            "report_content" : { "type" : "attachment" },
+            "file_name": { "type": "string" },
+            "file_size": { "type": "string" },
+            "report_type": { "type": "string" },
+            "requirement": { "type": "string" },
+            "location": { "type": "geo_point" },
+            "updated_date": { "type": "date" }
+        }
+    }
+}'
